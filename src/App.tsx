54b4328ffca07779cca8b7f171cbee56d2c7c7ee
adiputra32 <adip3287@gmail.com>
2024-01-25 09:31:49 +0800

    fix: fixing hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import { RouterProvider } from "react-router-dom";
+import { HashRouter, RouterProvider } from "react-router-dom";
 import { routes } from "./routes";
 
 function App() {
-  return <RouterProvider router={routes} />;
+  return (
+    <HashRouter>
+      <RouterProvider router={routes} />;
+    </HashRouter>
+  );
 }
 
 export default App;
